98bd3330ef8942f550bfdc19292582af04dd944e
AkramGafurov <akramminsk@yander.ru>
2020-10-14 07:38:55 +0300

    feat: finish brakets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let memory = str;
+for(let j = 0;j<str.length; j++){
+  for (let i=0;i<bracketsConfig.length; i++){
+    let pattern = bracketsConfig[i].join('');
+    if(memory.includes(pattern)){
+      memory = memory.replace(pattern,'')
+      if (memory.length==0){
+        return true
+      }
+    }
+  }
+} 
+
+return false;
+
 }
+
+// console.log(check('()', [['(', ')']]));
+// console.log(check('((()))()', [['(', ')']]));
+// console.log(check('())(', [['(', ')']]));
+// console.log(check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]));
+// console.log(check('[(])', [['(', ')'], ['[', ']']]));
+// console.log(check('[]()', [['(', ')'], ['[', ']']]));
+// console.log(check('[]()(', [['(', ')'], ['[', ']']]));
+// // check('[]()', [['(', ')'], ['[', ']']]) // -> true
+// // check('[]()(', [['(', ')'], ['[', ']']]) // -> false
+
+// // special case: opening and closing bracket can be the same :)
+
+// // check('||', [['|', '|']]) // -> true
+// // check('|()|', [['(', ')'], ['|', '|']]) // -> true
+// console.log(check('|(|)', [['(', ')'], ['|', '|']]));
+// // check('|(|)', [['(', ')'], ['|', '|']]) // -> false
+// // check('|()|(||)||', [['(', ')'], ['|', '|']]) // -> true
